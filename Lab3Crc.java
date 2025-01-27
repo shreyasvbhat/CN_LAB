fb3bd78cff0383fe8add26cd22d6ca0619b719e1
shreyasvbhat <udupi.shreyas.bhat@gmail.com>
2025-01-27 14:45:43 +0530

    changed crc code

--- Lab3Crc.java
+++ Lab3Crc.java
@@ -1,76 +1,46 @@
 import java.util.*;
 
-class Lab3Crc {
-        public static String xor(String a, String b)
-        {
-                String result = "";
-                int n = b.length();
-                for (int i = 1; i < n; i++) {
-                        if (a.charAt(i) == b.charAt(i))
-                                result += "0";
-                        else
-                                result += "1";
+public class Lab3Crc {
+    public static String divide(char[] divident, char[] divisor) {
+        int n = divident.length, m = divisor.length;
+
+        for (int i = 0; i < n; i++) {
+            if (divident[i] == '1') {
+                if (n - i < m)
+                    break;
+
+                for (int j = 0; j < m; j++) {
+                    divident[i + j] = divisor[j] == '0' ? divident[i + j] : divident[i + j] == '1' ? '0' : '1';
                 }
-
-                return result;
+            }
         }
 
-        public static String Mod2Div(String dividend, String divisor)
-        {
-                int curr = divisor.length();
-                String tmp = dividend.substring(0, curr);
-                int n = dividend.length();
+        // returning remainder
+        return new String(divident).substring(n - m + 1);
+    }
 
-                while (curr < n) {
-                        if (tmp.charAt(0) == '1')
-                                tmp = xor(divisor, tmp) + dividend.charAt(curr);
-                        else
-                                tmp= tmp + dividend.charAt(curr);
-                        curr++;
-                }
+    public static String encode(String data, String key) {
+        // data + remainder
+        return data + divide((data + "0".repeat(key.length() - 1)).toCharArray(), key.toCharArray());
+    }
 
-                if (tmp.charAt(0) == '1')
-                        tmp = xor(divisor, tmp);
+    public static boolean decode(String encodedData, String key) {
+        // returns true if remainder contains all zeros(i.e, error free data)
+        return divide(encodedData.toCharArray(), key.toCharArray()).contains("1");
+    }
 
-                return tmp;
-        }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        public static void EncodeData(String data, String key)
-        {
-                int len = key.length();
-                String appended_data = data + "0".repeat(len-1);
-                String remainder = Mod2Div(appended_data, key);
+        System.out.println("Enter binary key : ");
+        String key = sc.next();
+        System.out.println("Enter binary data :");
 
-                String codeword = data + remainder;
-                System.out.println("Remainder : " + remainder);
-                System.out.println( "Encoded Data (Data + Remainder) :" + codeword + "\n");
-        }
+        // encode data
+        System.out.println("Encoded data : " + encode(sc.next(), key));
 
-        static void Receiver(String data, String key)
-        {
-                String currxor = Mod2Div(data, key);
-
-                if (currxor.contains("1")) {
-                        System.out.println("There is some error in data");
-                } else {
-                        System.out.println("Correct message received");
-                }
-        }
-        public static void main(String[] args)
-        {
-                Scanner sc=new Scanner(System.in);
-                System.out.println("Enter the data in binary format: ");
-                String data =sc.next();
-
-                System.out.println("Enter the key in binary format: ");
-                String key = sc.next();
-
-                System.out.println("\nSender side...");
-                EncodeData(data, key);
-
-                System.out.println("Enter data received :");
-                data=sc.next();
-                Receiver(data,key);
-                sc.close();
-}
+        // decode data
+        System.out.println("Enter binary encoded data : ");
+        System.out.println(decode(sc.next(), key) ? "Error in the data" : "Data is error free");
+    }
 }
